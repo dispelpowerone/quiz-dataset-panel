f517985c07ab004d3978aee35147b62496c36a09
Dmitry Vasilyev <dispelpowerone@gmail.com>
2026-02-01 08:02:15 +0900

    Nit config fixes

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    https: false,
+    host: true // ensures it listens on the network IP
+  },
   plugins: [react()],
 })
